b0b87504f2d8e4182bb4d7641fd61b9f09f54461
Tom Wood <thomas.a.wood@gmail.com>
2013-09-26 23:14:07 -0400

    singletonSet, contains, union, intersect

--- progfun-funsets/src/main/scala/funsets/FunSets.scala
+++ progfun-funsets/src/main/scala/funsets/FunSets.scala
@@ -20,19 +20,19 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-  def singletonSet(elem: Int): Set = ???
+  def singletonSet(elem: Int): Set = (x: Int) => x == elem
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: Set, t: Set): Set = ???
+  def union(s: Set, t: Set): Set = (x : Int) => contains(s, x) || contains(t, x)
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: Set, t: Set): Set = ???
+  def intersect(s: Set, t: Set): Set = (x: Int) => contains(s, x) && contains(t, x)
 
   /**
    * Returns the difference of the two given sets,
--- progfun-funsets/src/test/scala/funsets/FunSetSuite.scala
+++ progfun-funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -86,7 +86,7 @@ class FunSetSuite extends FunSuite {
    * Once you finish your implementation of "singletonSet", exchange the
    * function "ignore" by "test".
    */
-  ignore("singletonSet(1) contains 1") {
+  test("singletonSet(1) contains 1") {
     
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
@@ -101,12 +101,21 @@ class FunSetSuite extends FunSuite {
     }
   }
 
-  ignore("union contains all elements") {
+  test("union contains all elements") {
     new TestSets {
       val s = union(s1, s2)
       assert(contains(s, 1), "Union 1")
       assert(contains(s, 2), "Union 2")
       assert(!contains(s, 3), "Union 3")
+    }
+  }
+  
+  test("intersect") {
+    new TestSets {
+      val s = union(s1, s2)
+      assert(! contains(intersect(s, s3), 1), "Intersect 1")
+      assert(contains(intersect(s, s1), 1), "Intersect 2")
+      assert(! contains(intersect(s, s2), 1), "Intersect 3")
     }
   }
 }
